cc6269c17938aca2c92f6ee599abc744bf777ec5
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2022-04-30 12:02:49 -0300

    add testes withdrawShouldDecreaseBalanceWhenSufficientBalance & withDrawShouldThrowExceptionInsufficientBalance

--- src/tests/entities/AccountTests.java
+++ src/tests/entities/AccountTests.java
@@ -10,39 +10,64 @@ public class AccountTests {
 
 	@Test
 	public void depositShouldIncreaseBalanceAndDiscountFeeWhenPositiveAmount() {
-		
+
 		double amount = 200.0;
 		double expectedValue = 196.0;
 		Account acc = AccountFactory.createEmptyAccount();
-		
+
 		acc.deposit(amount);
-		
+
 		Assertions.assertEquals(expectedValue, acc.getBalance());
 	}
-	
+
 	@Test
 	public void depositShouldDoNothingWhenNegativeAmount() {
-		
+
 		double expectedValue = 100.0;
 		Account acc = AccountFactory.createAccount(expectedValue);
 		double amount = -200.0;
-		
+
 		acc.deposit(amount);
-		
-		Assertions.assertEquals(expectedValue, acc.getBalance());			
+
+		Assertions.assertEquals(expectedValue, acc.getBalance());
 	}
-	
+
 	@Test
 	public void fullWithdrawShouldClearBalanceAndReturnFullBalance() {
-		
+
 		double expectedValue = 0.0;
 		double initialBalance = 100.0;
 		Account acc = AccountFactory.createAccount(initialBalance);
-		
+
 		double result = acc.fullWithdraw();
-		
+
 		Assertions.assertTrue(expectedValue == acc.getBalance());
 		Assertions.assertTrue(result == initialBalance);
 	}
-	
+
+	@Test
+	public void withdrawShouldDecreaseBalanceWhenSufficientBalance() {
+
+		double initialBalance = 800.0;
+		double valueToWithdraw = 500.0;
+		double expectedValue = 300.0;
+		Account acc = AccountFactory.createAccount(initialBalance);
+
+		acc.withdraw(valueToWithdraw);
+
+		Assertions.assertEquals(expectedValue, acc.getBalance());
+	}
+
+	@Test
+	public void withDrawShouldThrowExceptionInsufficientBalance() {
+
+		double initialBalance = 800.0;
+		double valueToWithdraw = 850.0;
+
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			Account acc = AccountFactory.createAccount(initialBalance);
+			acc.withdraw(valueToWithdraw);
+		});
+	}
+
 }
