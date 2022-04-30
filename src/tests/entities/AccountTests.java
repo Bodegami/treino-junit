0fddd5aa2a19616f69386c5a59bb8ca3591b6fcb
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2022-04-30 00:45:26 -0300

    add test fullWithdrawShouldClearBalanceAndReturnFullBalance

--- src/tests/entities/AccountTests.java
+++ src/tests/entities/AccountTests.java
@@ -31,6 +31,18 @@ public class AccountTests {
 		
 		Assertions.assertEquals(expectedValue, acc.getBalance());			
 	}
-
+	
+	@Test
+	public void fullWithdrawShouldClearBalanceAndReturnFullBalance() {
+		
+		double expectedValue = 0.0;
+		double initialBalance = 100.0;
+		Account acc = AccountFactory.createAccount(initialBalance);
+		
+		double result = acc.fullWithdraw();
+		
+		Assertions.assertTrue(expectedValue == acc.getBalance());
+		Assertions.assertTrue(result == initialBalance);
+	}
 	
 }
